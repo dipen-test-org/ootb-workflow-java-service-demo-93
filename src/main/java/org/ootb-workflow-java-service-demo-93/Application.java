961b84489425101df18cf74b7081b9f02fb8cba5
dipendra.bhatt@harness.io <>
2026-07-16 10:36:27 +0000

    Project init

---
+++ src/main/java/org/ootb-workflow-java-service-demo-93/Application.java
@@ -0,0 +1,13 @@
+package org.ootb-workflow-java-service-demo-93;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
